76250411ac37070075f8616cd7300491771f2743
D000ben000 <764770837@qq.com>
2016-08-13 00:07:10 +0800

    20160812 update for apart picture to same small size

--- cloud_detection/clean_data.py
+++ cloud_detection/clean_data.py
@@ -1,22 +1,37 @@
 from PIL import Image
 
 
-class clean_data():
-    def read_picture(self):
-        try:
-            im = Image.open('data/test.jpg')
-            gray_img = im.conver("L")
-        except:
-            print "read picture failure!"
-
+def read_picture():
+    try:
+        im = Image.open('data/test.jpg')
+        gray_img = im.convert("L")
         return gray_img
+    except IOError, e:
+        print "read picture failure!", "--", e
+        return None
 
-    def apart_picture_into_size(self, data, size = 25):
-        pass
+
+def save_train_data(tran_data, name):
+    """
+    :param tran_data:
+    :param name:
+    :return:
+    """
+    pass
+
+
+def apart_picture_into_size(size=25):
+    read_img = read_picture()
+    if read_img is None:
+        print "read_img is None!"
+
+    else:
+        picture_size = read_img.size
 
 
 def main():
-    pass
+    apart_picture_into_size()
+
 
 if __name__ == '__main__':
     main()
